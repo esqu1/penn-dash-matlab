f19f9ae74747a45b88c4cd38909a98e295468696
esqu1 <brandbest1@gmail.com>
2017-04-23 22:42:55 -0400

    starting laundry

--- PennDash.m
+++ PennDash.m
@@ -1,13 +1,18 @@
 function PennDash
+    RETURNTEXT = 'Return to Menu';
     f = figure('Visible','off','Position', [500 500 500 500]);
-    %button1 = uicontrol('Style','PushButton','Units','normalized','Position',[.5 .5 .4 .4],'String','Dining Halls');
-    
     title = uicontrol('Style','text','Units','normalized',...
                       'Position',[.1 .5 .8 .5],'String','Welcome to Penn Dash!',...
                       'FontSize',20);
     dining = uicontrol('Style','PushButton','Units','normalized',...
                        'Position',[.1 .7 .2 .1],'String','Dining Halls',...
                        'Callback',@Dining);
+                   
+    laundry = uicontrol('Style','PushButton','Units','normalized',...
+                        'Position',[.4 .7 .2 .1], 'String','Laundry',...
+                        'Callback',@Laundry);
+                   
+    central = {title dining laundry};
     movegui(f,'center');
     f.Visible = 'on';
     
@@ -16,16 +21,13 @@ function PennDash
     end
 
     function Dining(source,eventData)
-        title.Visible = 'off';
-        dining.Visible = 'off';
-        diningUpdate;
-    end
-
-
-
-    function diningUpdate
-        dining = webread('http://api.pennlabs.org/dining/venues');
-        venues = dining.document.venue;
+        dining.Enable = 'off';
+        for i=1:length(central)
+            central{i}.Visible = 'off';
+        end
+        d = webread('http://api.pennlabs.org/dining/venues');
+        dining.Enable = 'on';
+        venues = d.document.venue;
         hours = '';
         names = cell(1,1);
         % Need to update this with necessary buttons
@@ -33,10 +35,22 @@ function PennDash
             names{i} = venues{i}.name;
         end
         h = uicontrol('Style','text','Units','normalized',...
-                      'Position',[.5 .5 .3 .3],'String',hours);
+                      'Position',[.5 .5 .4 .4],'String',hours);
         hall = uicontrol('Style','listbox','Units','normalized',...
                          'Position',[.1 .1 .3 .3],'String',names,'Callback',@updateHours,...
                          'UserData',struct('hours',h,'names',names));
+        returnToMenu = uicontrol('Style','pushButton', 'Units', 'normalized',...
+                                 'Position', [.05 .8 .2 .1], 'String', RETURNTEXT,...
+                                 'CallBack', @backtoMenu);
+        f.Visible = 'on';
+        function backtoMenu(source,eventData)
+            h.Visible = 'off';
+            hall.Visible = 'off';
+            returnToMenu.Visible = 'off';
+            for i=1:length(central)
+                central{i}.Visible = 'on';
+            end
+        end
                      
         function updateHours(source,eventData)
             times = '';
@@ -47,7 +61,7 @@ function PennDash
                 return
             end
             for i=1:length(hour)
-                time = hour(i)
+                time = hour(i);
                 try
                     open = time.open;
                     closed = time.close;
@@ -59,4 +73,30 @@ function PennDash
             h.String = times;
         end
     end
+
+    function Laundry(source,eventData)
+        laundry.Enable = 'off';
+        for i=1:length(central)
+            central{i}.Visible = 'off';
+        end
+        d = webread('http://api.pennlabs.org/laundry/halls');
+        laundry.Enable = 'on';
+        halls = d.halls;
+        for i=1:length(halls)
+            n = strsplit(halls(i).name, '-');
+            halls(i).building = n{1};
+        end
+        listOfBuildings = unique({halls.building});        
+        l = uicontrol('Style','text','Units','normalized',...
+                      'Position',[.5 .5 .4 .4],'String','');
+        building = uicontrol('Style','listbox','Units','normalized',...
+                 'Position',[.1 .1 .3 .3],'String',listOfBuildings,'Callback',@updateHours,...
+                 'UserData',listOfBuildings);
+        
+        function updateLaundry(source,eventData)
+            
+        end    
+    end
+
+
 end
